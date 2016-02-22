bfe017be412f7ed6e4d0e1a55e2468644bdf8de4
Max Leiter <mleiter18@priorypanther.com>
2016-02-21 21:27:07 -0800

    Updates github issue regex

--- index.js
+++ index.js
@@ -126,7 +126,7 @@ bot.addListener("message", function(from, to, text) {
         //Just a message
     } else {
         if (text.indexOf("#") > -1 && from.toLowerCase().indexOf("github") === -1) {
-            var issues = text.match(/#([0-9]*)/g);
+            var issues = text.match(/#([0-9]+)/g);
             issues.forEach(function (issue) {
                 console.log(issue);
                 var issueNumber = issue.substr(1);
