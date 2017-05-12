f5bf4d15880e174eb218faa0257f30056c8ad2f1
Max Leiter <mleiter18@priorypanther.com>
2017-05-12 11:13:11 -0700

    Update to lounge's eslint: removed no-else-return and changed no-shadow

--- plugins/admin.js
+++ plugins/admin.js
@@ -1,22 +1,26 @@
+"use strict";
+
 var commands = function(bot, options, action) {
-	if (options.owners.indexOf(action.nick) < 0) return;
+	if (options.owners.indexOf(action.nick) < 0) {
+		return;
+	}
 	if (action.message.startsWith(options.botName)) {
 		let command = action.message.split(" ");
 		command.shift();
 		if (command[0] === "join") {
 			bot.join(command[1]);
 		} else if (command[0] === "part" || command[0] === "leave" || command[0] === "exit") {
-			if (command[1] == 'undefined' || command[1] === null) {
+			if (command[1] === "undefined" || command[1] === null) {
 				bot.part(action.target, action.nick + " asked me to leave.");
 			} else {
 				bot.part(command[1], action.nick + " asked me to leave.");
 			}
-		} else if (command[0] === "say" || command[0] === "echo") { // todo: doesn't work
+		} else if (command[0] === "say" || command[0] === "echo") {
 			let message = command;
 			message.shift();
 			bot.say(action.target, message.join(" "));
 		}
 	}
-}
+};
 
-module.exports = { commands };
+module.exports = {commands};
--- plugins/lounge.js
+++ plugins/lounge.js
@@ -1,9 +1,10 @@
+"use strict";
 var commands = function(bot, options, action) {
 	if (action.message === options.commandPrefix + "css") {
 		bot.say(action.target, "https://github.com/thelounge/lounge/wiki/CSS-Modifications");
 	} else if (action.message === options.commandPrefix + "rp") {
 		bot.say(action.target, "https://github.com/thelounge/lounge/wiki/Reverse-Proxies");
 	}
-}
+};
 
-module.exports = { commands };
+module.exports = {commands};
