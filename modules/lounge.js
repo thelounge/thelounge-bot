51315faca4f0958fa64cd43b30f0bbaa8203b5d3
Max Leiter <mleiter18@priorypanther.com>
2018-02-08 11:04:28 -0800

    Support targets in lounge commands (#17)

--- modules/lounge.js
+++ modules/lounge.js
@@ -1,17 +1,31 @@
 "use strict";
-var commands = function(bot, options, action) {
-	let target = action.target;
-	if (action.target === bot.user.nick) {
-		target = action.nick;
-	}
+let map;
 
-	if (action.message === options.commandPrefix + "css") {
-		bot.say(target, "https://github.com/thelounge/lounge/wiki/CSS-Modifications");
-	} else if (action.message === options.commandPrefix + "rp") {
-		bot.say(target, "https://github.com/thelounge/lounge/wiki/Reverse-Proxies");
-	} else if (action.message === options.commandPrefix + "diff") {
-		bot.say(target, "https://github.com/thelounge/lounge/wiki/Differences-between-The-Lounge-and-Shout");
-	}
+var init = function(options) {
+	map = setupMap(options);
+}
+
+var commands = function(bot, options, action) {
+	const message = action.message;
+	const words = message.split(" ");
+
+	let args = words.slice(); // copy words
+	args.splice(0, 1); // remove command
+	args = args.join(" ");
+
+	const target = args.split(/[ ,]/g).filter(Boolean).join(", ");
+	const url = map.get(words[0]);
+
+	bot.say(action.target, (target ? target + ": " + url : url));
 };
 
-module.exports = {commands};
+function setupMap(options) {
+	const temp = new Map();
+	temp.set(options.commandPrefix + "css", "https://github.com/thelounge/lounge/wiki/CSS-Modifications");
+	temp.set(options.commandPrefix + "wiki", "https://github.com/thelounge/lounge/wiki/");
+	temp.set(options.commandPrefix + "docs", "https://thelounge.github.io/docs/");
+	temp.set(options.commandPrefix + "releases", "https://github.com/thelounge/lounge/release");
+	return temp;
+}
+
+module.exports = {init, commands};
